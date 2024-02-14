db25384c0718a9879ede415db83d22293d903c6e
TusharPatwal <patwaltushar001@gmail.com>
2024-02-14 20:08:54 +0530

    removed sound button

--- script.js
+++ script.js
@@ -12,9 +12,6 @@ btn.addEventListener("click", () => {
             result.innerHTML = `
             <div class="word">
                     <h3>${inpWord}</h3>
-                    <button onclick="playSound()">
-                        <i class="fas fa-volume-up"></i>
-                    </button>
                 </div>
                 <div class="details">
                     <p>${data[0].meanings[0].partOfSpeech}</p>
